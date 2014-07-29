243a471918db9d74e12a98b1cdedf8ccf33acd16
nicolezhu <nzhu94@gmail.com>
2014-07-29 17:07:36 -0400

    media queries, fixed nav buttons

--- js/blerg.js
+++ js/blerg.js
@@ -31,6 +31,14 @@ browser_wide : function() {
 
   };
 
+
+<a class="toggle-bottom" data-name="danny" href="javascript:void(0);" mce_href="javascript:void(0);">Read the danny</a>
+
+function nextPerson() {
+  name = $('.toggle-bottom').data().name;
+  $('.toggle-bottom').html("Read " + name.upcase() + "'s story");
+}
+
 // start actual code in chorus
 function browserWide() {
   var window_width = $(window).width();
@@ -41,19 +49,72 @@ function browserWide() {
   });
 };
 
+function scrollToTop() {
+console.log( 'clicked' );
+  $('html,body').animate({scrollTop: $('.top-nav').offset().top}, 800);
+}
+
 function navButtons() {
   $('.chorus-snippet toggles a').parent().addClass('top-nav');
+  $('.chorus-snippet .top-nav toggles a').on('click', function(){
+    nextPerson();
+    nextBanner();
+  });
   var $button = $('toggles').clone(true);
   $('.toggle-bottom').parent().parent().addClass('bottom-nav').append($button);
   $('.chorus-snippet .bottom-nav toggles a').on('click', function(){
-    $('html,body').animate({scrollTop: $('.top-nav').offset().top}, 800);
+    scrollToTop();
+    nextPerson();
+    nextBanner();
   });
+}
+
+function nextPerson() {
+  var to_capitalize;
+  var name;
+  var current;
+  if (($(".bottom-nav").find('.toggle-selected').next().length == 0)) {
+    current = $('.bottom-nav').find('.toggle-selected').data().name;
+    name = 'Paige';
+  } else {
+    to_capitalize = $('.bottom-nav').find('.toggle-selected').next().data().name;
+    name = to_capitalize.charAt(0).toUpperCase() + to_capitalize.slice(1);
+  }
+  $('.quote-banner').removeClass(current);
+  $('.quote-banner a').html("Read " + name + "'s story");
+}
+
+function nextBanner() {
+  var name;
+  var current;
+  if ($(".bottom-nav").find('.toggle-selected').next().length == 0) {
+    console.log('end');
+    current = $('.bottom-nav').find('.toggle-selected').data().name;
+    name = 'leslie';
+  } else {
+    current = $('.quote-banner').attr('class').split(' ').pop();
+    name = $('.bottom-nav').find('.toggle-selected').next().data().name;
+  }
+  
+  $('.quote-banner').removeClass(current);
+  $('.quote-banner').addClass(name);
+}
+
+function firstBanner() {
+  var name = $('.bottom-nav').find('.toggle-selected').next().data().name;
+  $('.quote-banner').addClass(name);
 }
 
 $(document).ready(function() {
   browserWide();
   window.onresize = browserWide;
-  
   navButtons();
-  $('.toggle-bottom').html("Read more");
+  $('.bottom-nav em a').wrap("<div class='quote-banner'></div>").removeClass('toggle-bottom');
+  $('.quote-banner a').bind('click', scrollToTop);
+  nextPerson();
+  firstBanner();
+  $('.quote-banner').on('click', function() {
+    nextPerson();
+    nextBanner();
+  });
 });
